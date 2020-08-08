b020b9709111fc2e1dcd2758858c9e5e8a1dd13c
johnkirtley <kirtleyj16@gmail.com>
2020-08-08 15:51:34 -0700

    :fire: Added Project To Github

---
+++ bot.py
@@ -0,0 +1,88 @@
+import os
+import json
+from time import sleep
+import tweepy
+import random
+from datetime import datetime
+import config
+
+CONSUMER_KEY = config.CONSUMER_KEY
+CONSUMER_SECRET = config.CONSUMER_SECRET
+ACCESS_TOKEN = config.ACCESS_TOKEN
+ACCESS_TOKEN_SECRET = config.ACCESS_TOKEN_SECRET
+
+
+search_terms = ["Python", "JavaScript", "Node.js",
+                "ReactJS", "Web Development", "100daysofcode"]
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+
+        status = api.get_status(tweet.id)
+        favorited = status.favorited
+        sleep_interval = random.randint(200, 700)
+
+        if not favorited:
+            api.create_favorite(tweet.id)
+            now = datetime.now()
+            current_time = now.strftime("%H:%M")
+
+            print(
+                f"Time({current_time}): Liked Tweet ----- Next action in {sleep_interval // 60} minutes")
+
+            sleep(sleep_interval)
+
+        if not tweet.user.following:
+            tweet.user.follow()
+            now = datetime.now()
+            current_time = now.strftime("%H:%M")
+
+            print(
+                f"Time({current_time}): Followed: {tweet.user.screen_name} ----- Next action in {sleep_interval // 60} minutes")
+
+            sleep(sleep_interval)
+
+    def on_error(self, status):
+        print('Error detected')
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+tweet_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweet_listener)
+stream.filter(track=search_terms, is_async=True, languages=["en"])
+
+# with open('tweets.txt', 'r') as tweet_file:
+#     tweets = tweet_file.readlines()
+
+#     for tweet in tweets:
+#         tweet_interval = random.randint(500, 800)
+#         api.update_status(tweet)
+#         print(f"Tweet sent")
+
+#         sleep(tweet_interval)
+
+
+# for tweet in tweepy.Cursor(api.search, q="#javascript", lang='"en').items(500):
+
+#     status = api.get_status(tweet.id)
+#     favorited = status.favorited
+
+#     if not favorited:
+#         api.create_favorite(tweet.id)
+#         print("Liked Tweet")
+
+#     if not tweet.user.following:
+#         api.create_friendship(tweet.user.id)
+#         print(f"Followed {tweet.user.screen_name}")
+
+#     sleep(500)
