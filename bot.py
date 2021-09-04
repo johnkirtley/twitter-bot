9edd4a0486556e3b9efe4bc20f003986c8d2906f
johnkirtley <kirtleyj16@gmail.com>
2021-09-04 12:12:22 -0700

    Update

--- bot.py
+++ bot.py
@@ -11,10 +11,10 @@ from datetime import datetime
 # ACCESS_TOKEN = config.ACCESS_TOKEN
 # ACCESS_TOKEN_SECRET = config.ACCESS_TOKEN_SECRET
 
-    CONSUMER_KEY = os.environ["CONSUMER_KEY"]
-    CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-    ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-    ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 
 def twitter_api():
     CONSUMER_KEY = os.environ["CONSUMER_KEY"]
